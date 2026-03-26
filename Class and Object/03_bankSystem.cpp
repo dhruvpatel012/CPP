2e1e410a7fc0b2c4413323ec5f91c3a7bc797913
dhruv365 <pateldhruv5524@gmail.com>
2026-03-26 22:54:02 +0530

    Bank_System

---
+++ Class and Object/03_bankSystem.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+class bankAccount
+{
+private:
+    string name;
+    int balance;
+
+public:
+    void createAccount()
+    {
+        cout << "Enter your name:  ";
+        cin >> name;
+
+        cout << "Enter Initial Balance: ";
+        cin >> balance;
+    }
+
+    void depositeAmount()
+    {
+
+        int amount;
+
+        cout << "\nEnter Deposite Amount: ";
+        cin >> amount;
+
+        balance += amount;
+    }
+
+    void withdrawAmount()
+    {
+
+        int amount;
+
+        cout << "\nEnter Withdraw Amount: ";
+        cin >> amount;
+
+        if (amount <= balance)
+        {
+            balance -= amount;
+        }
+        else
+        {
+            cout << "\nNot Enough Balance for Withdraw Amount..!!"<<endl;
+        }
+    }
+
+    void showBalance(){
+        cout<<"\nYour Name: "<<name<<endl;
+        cout<<"Balance: "<<balance;
+    }
+};
+
+int
+main()
+{
+    bankAccount user1;
+
+    user1.createAccount();
+    user1.depositeAmount();
+    user1.withdrawAmount();
+    user1.showBalance();
+
+
+    return 0;
+}
