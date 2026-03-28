096fba4715bf5fcb4259d1cf7906333d787124f4
dhruvpatel012 <swaminarayan0112@gmail.com>
2026-03-28 21:34:53 +0530

    even odd

---
+++ Switch_Case & Ternary_Op/Even_Odd_Ternary_Op.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int num;
+
+    cout<<"Enter Number: ";
+    cin>>num;
+
+    (num %2== 0)? cout<<"Even Number !!":cout<<"Odd Number !!";
+
+    return 0;
+}
