b3d97b64c2873acfc0c4cc99d801dd029fcaab9e
dhruvpatel012 <swaminarayan0112@gmail.com>
2026-03-29 22:35:43 +0530

    new

---
+++ Patterns/Left_Half_Pyramid.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int i, j, k, size;
+
+    cout << "Enter a size of row:";
+    cin >> size;
+
+    for (i = size; i >= 1; i--)
+    {
+        for (k = 1; k <= i - 1; k++)
+        {
+            cout << " ";
+        }
+
+        for (j = size; j >= i; j--)
+        {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
