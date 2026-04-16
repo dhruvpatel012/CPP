58ba91ffb48734438df27ba00cb4787013d616c2
dhruvpatel012 <swaminarayan0112@gmail.com>
2026-04-16 18:01:13 +0530

    edit in shallow

--- Constructor/7_ShallowCopy.cpp
+++ Constructor/7_ShallowCopy.cpp
@@ -16,12 +16,12 @@ int main()
 {
 
     Demo obj1(10);
-    cout << *obj1.data << endl; // output: 10
+    cout << *(obj1.data) << endl; // here * is used for data not for object and its give output: 10
 
     Demo obj2 = obj1;
-    *obj2.data = 20;
-    cout << *obj2.data << endl; // output: 20
-    cout << *obj1.data << endl; // output : 20
+    *(obj2.data) = 20;
+    cout << *(obj2.data) << endl; // output: 20
+    cout << *(obj1.data) << endl; // output : 20
 
     return 0;
 }
